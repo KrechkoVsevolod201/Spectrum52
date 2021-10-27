0bf799b84e669195979aae052568818df5bb9b09
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2021-10-27 05:33:47 +0400

    Вроде бы закончил, но работает криво, сутра ещё допилю, надеюсь будет +- норм

--- scale_func.py
+++ scale_func.py
@@ -5,7 +5,7 @@ class Scale():
         # read the image
         im = Image.open("Files\Lines.JPG")
         (width1, height1) = im.size
-        img = Image.open("Files\Risunok_58 first try.jpg")
+        img = Image.open("Files\Risunok.jpg")
         (width2, height2) = img.size
         a = width1 / width2
         b = height1 / height2
@@ -13,7 +13,7 @@ class Scale():
         b = round(b)
         print(a, b)
         width2 = width2 * a * 2
-        height2 = height2 * b * 2
+        height2 = height2 * b
         # image size
         size = (width2, height2)
         # resize image
